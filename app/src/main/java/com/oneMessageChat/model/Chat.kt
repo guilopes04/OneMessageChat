8f25d99398b011e96d30af0ed7f00fd1e968c401
Guilherme <guilhermelopes035@gmail.com>
2023-12-05 23:00:30 -0300

    feat: cria objeto chat

---
+++ app/src/main/java/com/oneMessageChat/model/Chat.kt
@@ -0,0 +1,16 @@
+package com.oneMessageChat.model
+
+import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity
+data class Chat (
+    @PrimaryKey
+    var id: String = "",
+    @NonNull
+    var message: String = ""
+): Parcelable
